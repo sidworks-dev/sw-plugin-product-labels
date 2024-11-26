caf85be4d0a2d6bbd03e23f0f2cb276cc5648bea
Jelle Siderius <jelle@hypershop.nl>
2024-11-26 20:21:56 +0100

    WIP

--- src/Resources/app/administration/src/module/sidworks-product-labels/page/sidworks-product-labels-detail/index.js
+++ src/Resources/app/administration/src/module/sidworks-product-labels/page/sidworks-product-labels-detail/index.js
@@ -1,7 +1,34 @@
+const { Component, Context } = Shopware;
 import template from './sidworks-product-labels-detail.html.twig';
-
-const { Component } = Shopware;
 
 Component.register('sidworks-product-labels-detail', {
     template,
-})
+
+    inject: ['repositoryFactory', 'conditionDataProviderService'],
+
+    data() {
+        return {
+            entity: null,
+            repository: null,
+            conditionContext: {},
+        };
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    methods: {
+        async createdComponent() {
+            // Fetch your entity
+            this.repository = this.repositoryFactory.create('customer');
+            this.entity = await this.repository.get(this.$route.params.id, Context.api);
+
+            // Setup the condition context (e.g., for your specific entity)
+            this.conditionContext = {
+                entity: this.entity,
+                parentCondition: null
+            };
+        },
+    },
+});
