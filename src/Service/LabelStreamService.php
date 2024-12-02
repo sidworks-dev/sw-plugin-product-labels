51a98656cc8fd3d55a1246ca0ee64f5f360f467d
Jelle Siderius <jelle@hypershop.nl>
2024-12-02 10:53:13 +0100

    WIP

--- src/Service/LabelStreamService.php
+++ src/Service/LabelStreamService.php
@@ -7,9 +7,12 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Struct\ArrayEntity;
 
 class LabelStreamService
 {
+    private const SIDWORKS_PRODUCT_LABELS_EXTENSION = 'sidworksProductLabels';
+
     public function __construct(
         private readonly EntityRepository $productLabelsRepository,
         private readonly ProductStreamBuilderInterface $productStreamBuilder,
@@ -44,5 +47,24 @@ class LabelStreamService
         }
 
         return $productLabelStreamItems;
+    }
+
+    public function applyLabelsToProduct($product, array $productLabelsStreamProducts): void
+    {
+        foreach ($productLabelsStreamProducts as $productLabelsStreamProduct) {
+            $streamProducts = $productLabelsStreamProduct['stream']->getIds();
+            $label = $productLabelsStreamProduct['label'];
+            $labelId = $label->getId();
+
+            if (!in_array($product->getId(), $streamProducts, true)) {
+                continue;
+            }
+
+            /** @var ArrayEntity $sidworksProductLabels */
+            $sidworksProductLabels = $product->getExtension(self::SIDWORKS_PRODUCT_LABELS_EXTENSION) ?? new ArrayEntity();
+            $sidworksProductLabels->set($labelId, $label);
+
+            $product->addExtension(self::SIDWORKS_PRODUCT_LABELS_EXTENSION, $sidworksProductLabels);
+        }
     }
 }
--- src/Storefront/Page/Product/Subscriber/PageSubscriber.php
+++ src/Storefront/Page/Product/Subscriber/PageSubscriber.php
@@ -5,22 +5,37 @@ namespace Sidworks\ProductLabels\Storefront\Page\Product\Subscriber;
 use Shopware\Core\Framework\Struct\ArrayEntity;
 use Shopware\Core\Content\Product\Events\ProductListingResultEvent;
 use Shopware\Core\Content\Product\Events\ProductSearchResultEvent;
+use Shopware\Storefront\Page\Product\ProductPageLoadedEvent;
 use Sidworks\ProductLabels\Service\LabelStreamService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PageSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-      private LabelStreamService $labelStreamService
-    ) {
-    }
+        private readonly LabelStreamService $labelStreamService
+    ) {}
 
     public static function getSubscribedEvents(): array
     {
         return [
+            ProductPageLoadedEvent::class => 'onProductPageLoadedResult',
             ProductListingResultEvent::class => 'onProductListingResult',
-            ProductSearchResultEvent::class => 'onProductListingResult',
+            ProductSearchResultEvent::class => 'onProductListingResult'
         ];
+    }
+
+    public function onProductPageLoadedResult(ProductPageLoadedEvent $event): void
+    {
+        $context = $event->getContext();
+        $productLabelsStreamProducts = $this->labelStreamService->getProductLabelStreamProducts($context);
+
+        if (empty($productLabelsStreamProducts)) {
+            return;
+        }
+
+        $product = $event->getPage()->getProduct();
+
+        $this->labelStreamService->applyLabelsToProduct($product, $productLabelsStreamProducts);
     }
 
     public function onProductListingResult(ProductListingResultEvent $event): void
@@ -32,23 +47,8 @@ class PageSubscriber implements EventSubscriberInterface
             return;
         }
 
-        $productEntities = $event->getResult()->getEntities();
-
-        foreach ($productLabelsStreamProducts as $productLabelsStreamProduct) {
-            $streamProducts = $productLabelsStreamProduct['stream']->getIds();
-            $label = $productLabelsStreamProduct['label'];
-            $labelId = $label->getId();
-
-            foreach ($productEntities as $productEntity) {
-                if (!in_array($productEntity->getId(), $streamProducts, true)) {
-                    continue;
-                }
-
-                $sidworksProductLabels = $productEntity->getExtension('sidworksProductLabels') ?? new ArrayEntity();
-                $sidworksProductLabels->set($labelId, $label);
-
-                $productEntity->addExtension('sidworksProductLabels', $sidworksProductLabels);
-            }
+        foreach ($event->getResult()->getEntities() as $productEntity) {
+            $this->labelStreamService->applyLabelsToProduct($productEntity, $productLabelsStreamProducts);
         }
     }
 }
