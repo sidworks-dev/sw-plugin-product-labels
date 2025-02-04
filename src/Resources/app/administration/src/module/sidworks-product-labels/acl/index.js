2e2e22720675e13088898cb37a4da1fe56932f94
Jelle Siderius <jelle@hypershop.nl>
2025-02-04 11:01:35 +0100

    WIP

--- src/Resources/app/administration/src/module/sidworks-product-labels/acl/index.js
+++ src/Resources/app/administration/src/module/sidworks-product-labels/acl/index.js
@@ -1,30 +1,30 @@
 Shopware.Service('privileges').addPrivilegeMappingEntry({
     category: 'permissions',
     parent: null,
-    key: 'sidworks_product_labnels',
+    key: 'sidworks_product_labels',
     roles: {
         viewer: {
-            privileges: ['sidworks_product_labnels:read'],
+            privileges: ['sidworks_product_labels:read'],
             dependencies: []
         },
         editor: {
             privileges: [
-                'sidworks_product_labnels:update',
-                'sidworks_product_labnels_sales_channel:create',
-                'sidworks_product_labnels_sales_channel:delete'
+                'sidworks_product_labels:update',
+                'sidworks_product_labels_sales_channel:create',
+                'sidworks_product_labels_sales_channel:delete'
             ],
             dependencies: []
         },
         creator: {
             privileges: [
-                'sidworks_product_labnels:create',
-                'sidworks_product_labnels_sales_channel:create',
-                'sidworks_product_labnels_sales_channel:delete'
+                'sidworks_product_labels:create',
+                'sidworks_product_labels_sales_channel:create',
+                'sidworks_product_labels_sales_channel:delete'
             ],
             dependencies: []
         },
         deleter: {
-            privileges: ['sidworks_product_labnels:delete'],
+            privileges: ['sidworks_product_labels:delete'],
             dependencies: []
         }
     }
